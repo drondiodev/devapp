e8dd7784b86927e6ed6fdbc58ca0682d224fbd26
David Rondio <103181452+drondiodev@users.noreply.github.com>
2024-06-07 12:19:16 +0200

    create next app with shadcn and geist font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html
+      lang="fr"
+      className={`min-h-screen bg-background antialiased ${GeistSans.variable} ${GeistMono.variable}`}
+    >
+      <body>{children}</body>
     </html>
   );
 }
